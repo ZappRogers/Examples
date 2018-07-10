633b68ac3459f657d87437caf4e6be363d83c97d
ZappRogers <41024121+ZappRogers@users.noreply.github.com>
2018-07-10 13:43:17 +0100

    Add files via upload

---
+++ ifstatementexample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public int score;
+
+void Update()
+{
+    if (score == 50)
+    {
+        Debug.Log("GREAT!");
+
+    }
+
+    else if (score == 25)
+    {
+        Debug.Log("GOOD");
+
+    }
+
+    else
+    {
+        Debug.Log("POOR");
+    }
+
+}
